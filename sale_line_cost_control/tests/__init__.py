dd3b7e58ce98327c1188eb504b24265bd596a88c
victorALGIOS <Algios2017>
2020-10-14 09:24:00 +0200

    puesta a 0

--- sale_line_cost_control/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import test_sale
-from . import test_set_sale_line_purchase_price
--- sale_line_cost_control/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import set_sale_line_purchase_price
